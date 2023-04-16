e8b30e785bb9153f7227a18271c17d3a281b7a74
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-16 16:46:38 +0700

    feat(login): properly handle login post request

--- src/routes/api/v1/login/index.ts
+++ src/routes/api/v1/login/index.ts
@@ -1,20 +1,8 @@
-import { FastifyPluginAsync } from 'fastify';
-import S from 'fluent-json-schema';
+import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import post from './post';
 
-const login = (async (fastify, opts): Promise<void> => {
-	const schema = {
-		body: S.object()
-			.prop('email', S.string().required())
-			.prop('password', S.string().required()),
-	};
-	fastify.post('/', { schema }, async function (request, reply) {
-		return {
-			apiVersion: '1.0',
-			data: {
-				hello: 'world',
-			},
-		};
-	});
-}) satisfies FastifyPluginAsync;
+const route = (async (fastify, opts): Promise<void> => {
+	post(fastify);
+}) satisfies FastifyPluginAsyncTypebox;
 
-export default login;
+export default route;
---
+++ src/routes/api/v1/login/post.ts
@@ -0,0 +1,67 @@
+import {
+	FastifyPluginAsyncTypebox,
+	Type,
+} from '@fastify/type-provider-typebox';
+import { FastifySchema } from 'fastify';
+import httpStatus = require('http-status');
+import apiResponder from '~/lib/services/api-responder';
+import apiResponseSchema from '~/lib/services/api-response-schema';
+import mongodbCollections from '~/lib/services/mongodb-collections';
+import * as bcrypt from 'bcrypt';
+
+const post = (async (fastify): Promise<void> => {
+	const schema = {
+		body: Type.Object({
+			email: Type.String(),
+			password: Type.String(),
+		}),
+		response: {
+			200: apiResponseSchema.ofData(Type.Object({})),
+			400: apiResponseSchema.ofError(),
+		},
+	} satisfies FastifySchema;
+
+	fastify.post('/', { schema }, async function (request, reply) {
+		const user = await mongodbCollections.users.findOne(
+			{ email: request.body.email },
+			{ limit: 1, projection: { password: 1 } }
+		);
+
+		if (!user) {
+			reply
+				.code(httpStatus.UNAUTHORIZED)
+				.type('application/json')
+				.send(
+					apiResponder.error({
+						code: httpStatus.UNAUTHORIZED,
+						message: 'User not found',
+					})
+				);
+			return;
+		}
+
+		if (!bcrypt.compare(request.body.password, user.password)) {
+			reply
+				.code(httpStatus.UNAUTHORIZED)
+				.type('application/json')
+				.send(
+					apiResponder.error({
+						code: httpStatus.UNAUTHORIZED,
+						message: 'Wrong password',
+					})
+				);
+			return;
+		}
+
+		reply
+			.code(httpStatus.OK)
+			.type('application/json')
+			.setSession(
+				{ user: { _id: user._id.toHexString() } },
+				{ expiresIn: '1d' }
+			)
+			.send(apiResponder.data({}));
+	});
+}) satisfies FastifyPluginAsyncTypebox;
+
+export default post;
