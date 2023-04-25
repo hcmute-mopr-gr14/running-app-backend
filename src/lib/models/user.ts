5663288e6723a0c01b4aaeebe881f7ebca3f85e5
DATTRAN2207 <datvu2207@gmail.com>
2023-04-26 00:25:00 +0700

    add properties to user model

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -2,4 +2,14 @@ export default interface User {
 	email: string;
 	password: string;
 	sessionToken?: string;
+	nickname: string;
+	level: number;
+	stepData: StepData[];
+}
+
+interface StepData {
+	runningSeconds: number;
+	stepCount: number;
+	calories: number;
+	date: string;
 }
