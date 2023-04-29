a103d8a2c7ddd42f4244ae72d36d901ad7ef81a2
DATTRAN2207 <datvu2207@gmail.com>
2023-04-30 01:11:11 +0700

    update api/home/get schema, reply

--- src/routes/api/v1/home/get.ts
+++ src/routes/api/v1/home/get.ts
@@ -12,27 +12,32 @@ import { ObjectId } from 'mongodb';
 const get = (async (fastify): Promise<void> => {
 	const schema = {
 		querystring: Type.Object({
-            email: Type.String(),
+			email: Type.String(),
 		}),
 		response: {
-			200: ApiResponseSchema.instance.ofData(Type.Object({
-				nickname: Type.String(),
-				runningLogs: Type.Array(Type.Object({
-					_id: Type.String(),
-					seconds: Type.Integer(),
-					steps: Type.Integer(),
-					distance: Type.Integer(),
-				})),
-			})),
+			200: ApiResponseSchema.instance.ofData(
+				Type.Object({
+					nickname: Type.String(),
+					runningLogs: Type.Array(
+						Type.Object({
+							_id: Type.String(),
+							seconds: Type.Integer(),
+							steps: Type.Integer(),
+							distance: Type.Integer(),
+							date: Type.String(),
+						})
+					),
+				})
+			),
 			400: ApiResponseSchema.instance.ofError(),
 		},
 	} satisfies FastifySchema;
 
 	fastify.get('/', { schema }, async function (request, reply) {
-        const user = await DbClient.instance.collections.users.findOne(
-            { email: request.query.email },
-            { projection: { nickname: 1, runningLogs: 1 } }
-        );
+		const user = await DbClient.instance.collections.users.findOne(
+			{ email: request.query.email },
+			{ projection: { nickname: 1, runningLogs: 1 } }
+		);
 
 		if (!user) {
 			reply
@@ -49,56 +54,64 @@ const get = (async (fastify): Promise<void> => {
 			reply
 				.code(httpStatus.INTERNAL_SERVER_ERROR)
 				.type('application/json')
-				.send(ApiResponder.instance.error({
-					code: 'httpStatus.INTERNAL_SERVER_ERROR',
-					message: 'RunningLogs not found',
-				}));
+				.send(
+					ApiResponder.instance.error({
+						code: 'httpStatus.INTERNAL_SERVER_ERROR',
+						message: 'RunningLogs not found',
+					})
+				);
 			return;
 		} else {
 			// Kiểm tra dữ liệu trong mảng runningLogs có dữ liệu của ngày hôm nay ?, nếu chưa thì tạo dữ liệu mặc định
 			const today = new Date();
-			const todayString = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
+			const todayString = `${today.getFullYear()}-${
+				today.getMonth() + 1
+			}-${today.getDate()}`;
 			let hasTodayLog = false;
 
 			for (const log of user.runningLogs) {
-    			const logDate = new Date(log._id.getTimestamp());
-    			const logDateString = `${logDate.getFullYear()}-${logDate.getMonth() + 1}-${logDate.getDate()}`;
-    			if (logDateString === todayString) {
-        			hasTodayLog = true;
-        			break;
-    			}
+				const logDate = new Date(log._id.getTimestamp());
+				const logDateString = `${logDate.getFullYear()}-${
+					logDate.getMonth() + 1
+				}-${logDate.getDate()}`;
+				if (logDateString === todayString) {
+					hasTodayLog = true;
+					break;
+				}
 			}
 
 			if (!hasTodayLog) {
-    			const defaultLog = {
-        		_id: new ObjectId(),
-        		seconds: 0,
-        		steps: 0,
-        		distance: 0,
-    		};
-			user.runningLogs.push(defaultLog);
+				const defaultLog = {
+					_id: new ObjectId(),
+					seconds: 0,
+					steps: 0,
+					distance: 0,
+				};
+				user.runningLogs.push(defaultLog);
 
-			// Cập nhật dữ liệu mặc định vào database
-			await DbClient.instance.collections.users.updateOne(
-				{ email: user.email },
-				{ $set: { runningLogs: user.runningLogs } }
+				// Cập nhật dữ liệu mặc định vào database
+				await DbClient.instance.collections.users.updateOne(
+					{ email: user.email },
+					{ $push: { runningLogs: defaultLog } }
 				);
 			}
 		}
-		
+
 		reply
-    		.code(httpStatus.OK)
-    		.type('application/json')
-    		.send(ApiResponder.instance.data({
-        		nickname: user.nickname,
-        		runningLogs: user.runningLogs.map(log => ({
-            		_id: log._id.toHexString(),
-            		seconds: log.seconds,
-            		steps: log.steps,
-            		distance: log.distance
-        		})),
-    		}));
-		});
-	}) satisfies FastifyPluginAsyncTypebox;
+			.code(httpStatus.OK)
+			.type('application/json')
+			.send(
+				ApiResponder.instance.data({
+					nickname: user.nickname,
+					runningLogs: user.runningLogs.map((log) =>
+						Object.assign(log, {
+							_id: log._id.toHexString(),
+							date: log._id.getTimestamp().toISOString(),
+						})
+					),
+				})
+			);
+	});
+}) satisfies FastifyPluginAsyncTypebox;
 
 export default get;
