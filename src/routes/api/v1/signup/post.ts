6c302e7b06892ab55fe4035c8b0abb022a20802c
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-04 20:16:49 +0700

    refactor(signup): format code

--- src/routes/api/v1/signup/post.ts
+++ src/routes/api/v1/signup/post.ts
@@ -1,6 +1,6 @@
 import {
-    FastifyPluginAsyncTypebox,
-    Type,
+	FastifyPluginAsyncTypebox,
+	Type,
 } from '@fastify/type-provider-typebox';
 import { FastifySchema } from 'fastify';
 import httpStatus = require('http-status');
@@ -11,58 +11,51 @@ import { ObjectId } from 'mongodb';
 import * as bcrypt from 'bcrypt';
 
 const post = (async (fastify): Promise<void> => {
-    const schema = {
-        body: Type.Object({
-            email: Type.String(),
-            password: Type.String(),
-            nickname: Type.String(),
-        }),
-        response: {
-            200: ApiResponseSchema.instance.ofData(Type.Object({})),
-            400: ApiResponseSchema.instance.ofError(),
-        },
-    } satisfies FastifySchema;
+	const schema = {
+		body: Type.Object({
+			email: Type.String(),
+			password: Type.String(),
+			nickname: Type.String(),
+		}),
+		response: {
+			200: ApiResponseSchema.instance.ofData(Type.Object({})),
+			400: ApiResponseSchema.instance.ofError(),
+		},
+	} satisfies FastifySchema;
 
-    fastify.post('/', { schema }, async function (request, reply) {
-        const existingUser = await DbClient.instance.collections.users.countDocuments(
-            { email: request.body.email },
-            { limit: 1 });
+	fastify.post('/', { schema }, async function (request, reply) {
+		const existingUser =
+			await DbClient.instance.collections.users.countDocuments(
+				{ email: request.body.email },
+				{ limit: 1 }
+			);
+		if (existingUser) {
+			reply.status(httpStatus.BAD_REQUEST).send(
+				ApiResponder.instance.error({
+					code: 'httpStatus.UNAUTHORIZED',
+					message: 'Email already exists',
+				})
+			);
+			return;
+		}
+		// Mã hóa mật khẩu
+		const hashedPassword = await bcrypt.hash(request.body.password, 12);
 
-        if (existingUser) {
-            reply
-                .code(httpStatus.BAD_REQUEST)
-                .type('application/json')
-                .send(
-                    ApiResponder.instance.error({
-                        code: 'httpStatus.UNAUTHORIZED',
-                        message: 'Email already exists',
-                    })
-                );
-            return;
-        }
-        // Mã hóa mật khẩu
-        const hashedPassword = await bcrypt.hash(request.body.password, 12);
+		// Thêm tài khoản vào mongodb
+		await DbClient.instance.collections.users.insertOne({
+			email: request.body.email,
+			password: hashedPassword,
+		});
 
-        // Thêm tài khoản vào mongodb
-        await DbClient.instance.collections.users.insertOne({
-            email: request.body.email,
-            password: hashedPassword,
-            nickname: request.body.nickname,
-            runningLogs: [{
-                _id: new ObjectId(),
-                seconds: 0,
-                steps: 0,
-                distance: 0,
-            }],
-        });
-		
-
-        reply
-            .code(httpStatus.OK)
-            .type('application/json')
-            .send(ApiResponder.instance.data({ message: 'User created successfully' }));
-
-    });
+		reply
+			.code(httpStatus.OK)
+			.type('application/json')
+			.send(
+				ApiResponder.instance.data({
+					message: 'User created successfully',
+				})
+			);
+	});
 }) satisfies FastifyPluginAsyncTypebox;
 
 export default post;
