142cf2ec9cfbaf736134c7946eb566177a47e368
DATTRAN2207 <datvu2207@gmail.com>
2023-05-09 22:15:12 +0700

    add api update userprofile

--- src/routes/api/v1/userprofile/index.ts
+++ src/routes/api/v1/userprofile/index.ts
@@ -1,8 +1,10 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import post from './post';
+import put from './put';
 
 const route = (async (fastify, opts): Promise<void> => {
 	post(fastify);
+	put(fastify);
 }) satisfies FastifyPluginAsyncTypebox;
 
 export default route;
