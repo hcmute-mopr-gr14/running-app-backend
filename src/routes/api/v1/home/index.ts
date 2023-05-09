77d0ca3f5febf84e380aa04a7201f055eb72c70d
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-10 03:51:30 +0700

    refactor: temporarily disable home route

--- src/routes/api/v1/home/index.ts
+++ src/routes/api/v1/home/index.ts
@@ -1,8 +1,8 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import get from './get';
+// import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+// import get from './get';
 
-const route = (async (fastify, opts): Promise<void> => {
-	get(fastify);
-}) satisfies FastifyPluginAsyncTypebox;
+// const route = (async (fastify, opts): Promise<void> => {
+// 	get(fastify);
+// }) satisfies FastifyPluginAsyncTypebox;
 
-export default route;
+// export default route;
