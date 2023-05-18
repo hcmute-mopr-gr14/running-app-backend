ed648e01d3968a17ef8a2a1e29894a0fa26e1261
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-19 05:29:47 +0700

    feat(withImageUrl): add transformation and default image url

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,8 +1,9 @@
 import User from '../models/user';
-import { v2 as cloudinary } from 'cloudinary';
+import { CommonTransformationOptions, v2 as cloudinary } from 'cloudinary';
 
 export function withImageUrl(
-	user: User
+	user: User,
+	transformation?: CommonTransformationOptions
 ): Omit<User, 'publicId' | 'version'> & { imageUrl: string } {
 	const clone = Object.assign(user, {
 		imageUrl: user.image
@@ -11,8 +12,9 @@ export function withImageUrl(
 					format: user.image.format,
 					resource_type: 'image',
 					secure: true,
+					transformation,
 			  })
-			: '',
+			: 'https://res.cloudinary.com/dymb8gidr/image/upload/v1683569585/running_app/6453b88a02adb65e9d167544_1683569589367.jpg',
 	});
 	delete (clone as any).image;
 	return clone;
